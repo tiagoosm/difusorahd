7cfd321f1db75ff507550654809ac7e19b114e63
tiagoosm <tiagoschad@gmail.com>
2026-07-25 15:45:02 -0300

    chore: scaffold Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Portal de Notícias</h1>
+      <p>Projeto inicializado. Próxima etapa: configurar Tailwind CSS.</p>
+    </div>
+  )
+}
+
+export default App
